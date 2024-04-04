e36c5699eeb7efdc4341dd333bf1ac2bd78f18dc
VarunSivamani <varunsivamani@gmail.com>
2024-04-04 21:38:03 +0530

    Components and DataBinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Project';
+
+  loaded_feature = 'recipe';
+
+  onNavigate(feature: string){
+    this.loaded_feature = feature;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,19 +7,24 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 
+    new Recipe('A Test Recipe1', 'This is simply a test1', 
     'https://kitchenofdebjani.com/wp-content/uploads/2023/04/easy-indian-chicken-curry-Recipe-for-beginners-Debjanir-rannaghar.jpg'
     ),
-    new Recipe('A Test Recipe', 'This is simply a test', 
+    new Recipe('A Test Recipe2', 'This is simply a test2', 
     'https://kitchenofdebjani.com/wp-content/uploads/2023/04/easy-indian-chicken-curry-Recipe-for-beginners-Debjanir-rannaghar.jpg'
     ),
-    new Recipe('A Test Recipe', 'This is simply a test', 
+    new Recipe('A Test Recipe3', 'This is simply a test3', 
     'https://kitchenofdebjani.com/wp-content/uploads/2023/04/easy-indian-chicken-curry-Recipe-for-beginners-Debjanir-rannaghar.jpg'
     ),
-    new Recipe('A Test Recipe', 'This is simply a test', 
+    new Recipe('A Test Recipe4', 'This is simply a test4', 
     'https://kitchenofdebjani.com/wp-content/uploads/2023/04/easy-indian-chicken-curry-Recipe-for-beginners-Debjanir-rannaghar.jpg'
     )
   ];
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
